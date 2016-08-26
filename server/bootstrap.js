6633047672457d7fdc8adf1d9bcab1e289886a02
soulness <soulness@outlook.com>
2016-08-27 01:36:22 +0300

    Добавил создание тестовых пользователей

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,28 @@
 import Moment from 'moment';
 import { Meteor } from 'meteor/meteor';
 import { Chats, Messages } from '../lib/collections';
+
+Meteor.startup(function() {
+    if (Meteor.users.find().count() != 0) return;
+
+    Accounts.createUserWithPhone({
+        phone: '+972501234567',
+        profile: {
+            name: 'My friend 1'
+        }
+    });
+
+    Accounts.createUserWithPhone({
+        phone: '+972501234568',
+        profile: {
+            name: 'My friend 2'
+        }
+    });
+
+    Accounts.createUserWithPhone({
+        phone: '+972501234569',
+        profile: {
+            name: 'My friend 3'
+        }
+    });
+});
